7fb567d2efb8439fe7c3034bfb1807a8b1c81c25
root <M00185@bpifrance.fr>
2025-06-23 16:32:47 +0200

    feat(Raphael YU)

--- src/data_skills_w_pandas.py
+++ src/data_skills_w_pandas.py
@@ -1,28 +1,34 @@
 # Import libraries
-
+import pandas as pd
+import os
 
 def main():
-    pass
     # Load data
-
+    path_customers = os.path.join("data", "input", "customer.csv")
+    path_order = os.path.join("data", "input", "order.csv")
+    
+    customers = pd.read_csv(path_customers)
+    orders = pd.read_csv(path_order)
 
     # Check if Nan values
 
 
     # Count Nan values per columns
-
+    ordersna = orders.isna()
+    for column in ordersna.columns:
+        ordersna[column].sum()
 
     # Get total price of all orders
-
+    orders.apply(lambda x: x["quantity"] * x["unit_price"], axis=1).sum()
 
     # Remove Nan values
-
+    orders.dropna(inplace=True, axis=1)
 
     # What is the category repartition
-
+    
 
     # Standardise categories
-
+    orders["category"] = orders["category"].str.title()
 
     # Get .fr and .com repartition among customers
 
--- src/prog_skills_w_python.py
+++ src/prog_skills_w_python.py
@@ -1,5 +1,9 @@
 # Imports
 
 
-def tri():
-    pass
+def tri(l: list) -> list:
+    for i in len(l):
+        if l[i] > l[i+1]:
+            tmp = l[i]
+            l[i] = l[i+1]
+            l[i+1] = tmp
