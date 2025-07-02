7a2821d8c5cd54404c52e5f8823fe42dd9403deb
root <M00185@bpifrance.fr>
2025-07-02 10:52:56 +0200

    Attempt to quick sort recursive

--- src/data_skills_w_pandas.py
+++ src/data_skills_w_pandas.py
@@ -1,31 +1,34 @@
 # Import libraries
-
+import pandas as pd
 
 def main():
     pass
     # Load data
+    df_order = pd.read_csv('./data/input/order.csv')
+    df_customer = pd.read_csv('./data/input/customer.csv')
 
 
     # Check if Nan values
-
+    df_order.isna()
 
     # Count Nan values per columns
-
+    df_order.isna().sum()
 
     # Get total price of all orders
-
+    df_order["price"] = df_order['unit_price'] * df_order['quantity']
+    df_order["price"].sum()
 
     # Remove Nan values
-
+    df_order = df_order.dropna()
 
     # What is the category repartition
-
+    df_order["category"].value_counts()
 
     # Standardise categories
-
+    df_order["category"].str.title().value_counts()
 
     # Get .fr and .com repartition among customers
-
+    df_customer
 
     # Get repartition of email provider
 
--- src/prog_skills_w_python.py
+++ src/prog_skills_w_python.py
@@ -1,5 +1,30 @@
 # Imports
+from typing import List
 
+def triSub(L, low, high):
+    low = 0
+    high = len(L) - 1
+    pivot = partition(L, low, high)
+    triSub(L, pivot + 1, high)
+    triSub(L, low, pivot - 1)
 
-def tri():
+def tri(L: List[int]):
+    """
+    Quick sort
+    """
     pass
+    
+
+def partition(L, low, high) -> int:
+    """
+    Determine the index for partition
+    """
+    i = low - 1
+    pivot = high
+    while i <= pivot:
+        if L[i] > L[pivot]:
+            L[i], L[pivot] = L[pivot], L[i]
+    L[i+1], L[pivot] = L[pivot], L[i+1]
+    return i + 1
+
+
